118bceda4f251595b71c2bd4e141a238d0494e4f
z-Wind <zpsyhapcst@gmail.com>
2023-01-24 22:26:22 +0800

    iced 版本升級至 0.7

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,24 @@
-use iced::{executor, Application, Clipboard, Command, Element, Settings, Subscription, Text};
+use iced::widget::canvas::{Cache, Cursor, Geometry};
+use iced::widget::{canvas, container};
+use iced::{alignment, application, executor, theme, window};
+use iced::{
+    Application, Color, Command, Element, Length, Rectangle, Settings, Subscription, Theme, Vector,
+};
 
 pub fn main() -> iced::Result {
-    let mut settings = Settings::default();
-    settings.window.always_on_top = false;
-    EyeProtect::run(settings)
+    EyeProtect::run(Settings {
+        window: window::Settings {
+            always_on_top: false,
+            position: window::Position::Centered,
+            ..window::Settings::default()
+        },
+        ..Settings::default()
+    })
 }
 
 struct EyeProtect {
     value: i32,
+    text: Cache,
 }
 
 #[derive(Debug, Clone)]
@@ -19,24 +30,32 @@ impl Application for EyeProtect {
     type Executor = executor::Default;
     type Message = Message;
     type Flags = ();
+    type Theme = Theme;
 
     fn new(_flags: ()) -> (EyeProtect, Command<Self::Message>) {
-        (EyeProtect { value: 20 }, Command::none())
+        let value = 20;
+        (
+            Self {
+                value,
+                text: Default::default(),
+            },
+            iced::window::set_mode(iced::window::Mode::Fullscreen),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Eye Protect")
     }
 
-    fn update(
-        &mut self,
-        message: Self::Message,
-        _clipboard: &mut Clipboard,
-    ) -> Command<Self::Message> {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
             Message::Tick => {
                 self.value -= 1;
+                self.text.clear();
             }
+        }
+        if self.value < 0 {
+            return window::close();
         }
 
         Command::none()
@@ -46,28 +65,57 @@ impl Application for EyeProtect {
         iced::time::every(std::time::Duration::from_millis(1000)).map(|_| Message::Tick)
     }
 
-    fn view(&mut self) -> Element<Self::Message> {
-        let mut text = Text::new(format!("{:02} s", self.value));
-        text = text
-            .color(iced::Color::WHITE)
-            .size(150)
-            .height(iced::Length::Fill)
-            .width(iced::Length::Fill)
-            .horizontal_alignment(iced::HorizontalAlignment::Center)
-            .vertical_alignment(iced::VerticalAlignment::Center);
-        text.into()
+    fn view(&self) -> Element<Self::Message> {
+        let canvas = canvas(self as &Self)
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        container(canvas)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 
-    fn mode(&self) -> iced::window::Mode {
-        iced::window::Mode::Fullscreen
-        // iced::window::Mode::Windowed
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 
-    fn background_color(&self) -> iced::Color {
-        iced::Color::BLACK
-    }
+    fn style(&self) -> theme::Application {
+        fn dark_background(_theme: &Theme) -> application::Appearance {
+            application::Appearance {
+                background_color: Color::BLACK,
+                text_color: Color::WHITE,
+            }
+        }
 
-    fn should_exit(&self) -> bool {
-        self.value <= 0
+        theme::Application::from(dark_background as fn(&Theme) -> _)
+    }
+}
+
+impl<Message> canvas::Program<Message> for EyeProtect {
+    type State = ();
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        _theme: &iced::Theme,
+        bounds: Rectangle,
+        _cursor: Cursor,
+    ) -> Vec<Geometry> {
+        let text = self.text.draw(bounds.size(), |frame| {
+            let center = frame.center();
+            frame.translate(Vector::new(center.x, center.y));
+
+            frame.fill_text(canvas::Text {
+                content: format!("{:02} s", self.value),
+                color: Color::WHITE,
+                size: 150.0,
+                horizontal_alignment: alignment::Horizontal::Center,
+                vertical_alignment: alignment::Vertical::Center,
+                ..canvas::Text::default()
+            });
+        });
+
+        vec![text]
     }
 }
